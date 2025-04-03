1e1e5ccd94b284a02b2d25000dfc9e17a5480595
Mohammad Dawas <mohammad.dawas@arangodb.com>
2025-04-03 11:08:41 +0200

    finish section 2.6

--- src/llm_dev/ch2/data_loader.py
+++ src/llm_dev/ch2/data_loader.py
@@ -1,4 +1,5 @@
 import torch
+import tiktoken
 from torch.utils.data import Dataset, DataLoader
 
 
@@ -22,4 +23,22 @@ class GPTDatasetV1(Dataset):
         return self.input_ids[idx], self.target_ids[idx]
 
 
-def create_dataloader_v1(txt)
+def create_dataloader_v1(
+    txt,
+    batch_size=4,
+    max_length=256,
+    stride=128,
+    shuffle=True,
+    drop_last=True,
+    num_workers=0,
+):
+    tokenizer = tiktoken.get_encoding("gpt2")
+    dataset = GPTDatasetV1(txt, tokenizer, max_length, stride)
+    dataloader = DataLoader(
+        dataset=dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        num_workers=num_workers,
+        drop_last=drop_last,
+    )
+    return dataloader
